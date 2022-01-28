ed384462580293ea4766f6c09b33f4398b0f311b
Jordan-Michael <31947420+jmvbogo@users.noreply.github.com>
2022-01-28 18:54:49 -0500

    Add files via upload
    Added options

--- PythonPlayground/Evens and Odds.py
+++ PythonPlayground/Evens and Odds.py
@@ -1,12 +1,30 @@
 import numpy as np
+import sys
 
 def EvensOdds():
-    nmbrs=np.random.randint(100, size=500)
+    option=int(input('''Check for Even and Odds:
+    1.) Run random numbers
+    2.) Exit program
+    Choice: '''))
+    
+    if option==1:
+        nmbrs=np.random.randint(100, size=500)
 
-    even=[n for n in nmbrs if n%2==0]
-    odd=[n for n in nmbrs if n%2!=0]
-    
-    print('Even numbers: \n', even)
-    print('Odd numbers: \n',odd)
-    
-EvensOdds()
+        even=[n for n in nmbrs if n%2==0]
+        odd=[n for n in nmbrs if n%2!=0]
+        
+        print('Even numbers: \n', even)
+        print('Odd numbers: \n',odd)
+        print('\n')
+        
+        return True
+    elif option==2:
+        sys.exit()
+    else:
+        print('*Please enter the correct choice*')
+
+def main():
+    while True:
+        EvensOdds()
+        
+main()
